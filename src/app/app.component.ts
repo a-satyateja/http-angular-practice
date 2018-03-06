daa6b1b69dc43007b6a414dcb91b03365d59f642
a-satyateja <30695365+a-satyateja@users.noreply.github.com>
2018-03-06 17:00:35 +0530

    asda

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string;
+  constructor(private httpClient: HttpClient) {}
+  onNameKeyUp( event: any) {
+    this.name = event.target.value;
+  }
+  getProfile() {
+    console.log(this.name);
+  }
+}
